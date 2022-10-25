51d7289d7cf3350c698c3654a0020afb7e6cc6d7
이희수 <gmltn3113@naver.com>
2022-10-25 21:10:51 +0900

    chapter 1 - 출력

---
+++ src/main/java/j01_출력/PrintTest1.java
@@ -0,0 +1,35 @@
+package src.main.java.j01_출력;
+
+/**
+ * @
+ *
+ *
+ */
+
+
+
+public class PrintTest1 {
+
+    public static void main(String[] args) {
+
+//  한줄 주석
+/*  = 여러줄 주석*/
+
+/**
+ *
+ *  클래스 또는 메소드의 역할을 명시 또는 설명하기 위한 주석
+ */
+
+
+    //문자열 출력
+        System.out.print("  이름 : \n ");
+        System.out.println("이희수");
+        System.out.print("  주소:");
+        System.out.println("  동래구 사직동:");
+
+
+
+    }
+
+
+}
